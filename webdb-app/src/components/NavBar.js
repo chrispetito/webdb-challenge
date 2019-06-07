9498e7d45bfbfa0b2296b54c8b2c2194b104d90a
chrispetito <christopherpetito@gmail.com>
2019-06-07 11:10:48 -0700

    add react app

---
+++ webdb-app/src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../index.css";
+
+class NavBar extends React.Component {
+  render() {
+    return (
+      <div className="navbar">
+        <h1>Web DB Sprint Challenge</h1>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
